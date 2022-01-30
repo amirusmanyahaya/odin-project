9568558fb81813ef51977a6d8e887abf0f11bfa6
Yahaya Usman <amirusman20@gmail.com>
2022-01-30 01:51:41 +0100

    inital commit

---
+++ exercise/restuarant_page/dist/index.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Hello form initial commit')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsd0MiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZXN0dWFyYW50X3BhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ0hlbGxvIGZvcm0gaW5pdGlhbCBjb21taXQnKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
