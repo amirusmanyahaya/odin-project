6b3d03ebc7d7f9dcf0d4bfcaaa0ba562be4f0614
Yahaya Usman <amirusman20@gmail.com>
2024-01-30 23:59:33 +0100

    Added functionality for playing

--- exercise/rock-paper-scissors/scricpt.js
+++ exercise/rock-paper-scissors/scricpt.js
@@ -1,5 +1,24 @@
 const CHOICE_LIST = ["Rock", "Paper", "Scissors"]
+
 function getComputerChoice() {
-    let randIndex = Math.floor(Math.random() * 3)
+    const randIndex = Math.floor(Math.random() * 3)
     return CHOICE_LIST[randIndex]
+}
+
+function getGameResult(playerChoice, computerChoice) {
+
+    // computes the logic for determining the winner of one
+    // round of rock, paper, scissors.
+    return "Player"
+}
+
+function playRound(playerChoice, computerChoice) {
+    // play game
+    // determine who wins
+    // log winner
+    console.log(`Player played: ${playerChoice}`)
+    console.log(`Computer played: ${playerChoice}`)
+    const result = getGameResult(playerChoice,computerChoice)
+    console.log(`${playerChoice} won the game`)
+
 }
